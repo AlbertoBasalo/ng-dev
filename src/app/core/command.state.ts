862b515617cce0a5b7b93160077cae0e0f2a0c85
Alberto Basalo <albertobasalo@hotmail.com>
2023-06-04 18:23:45 +0200

    refactor: command state

---
+++ src/app/core/command.state.ts
@@ -0,0 +1,50 @@
+import { computed, signal } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export class CommandState<T> {
+  readonly isWorking = signal(false);
+  readonly result = signal<T | T[] | null>(null);
+  readonly error = signal<object | null>(null);
+  // derived states
+  readonly hasError = computed(() => this.error() !== null);
+  readonly isCompleted = computed(() => {
+    if (this.isWorking()) return false;
+    if (this.error()) return false;
+    return true;
+  });
+  readonly hasContent = computed(() => {
+    const result = this.result();
+    if (!result) return false;
+    if (Array.isArray(result)) return result.length > 0;
+    return true;
+  });
+  readonly errorMessage = computed(() => {
+    const e = this.error();
+    if (!e) return '';
+    return e instanceof Error ? e.message : JSON.stringify(e);
+  });
+
+  execute(command$: Observable<T | T[]>) {
+    this.start();
+    command$.subscribe({
+      next: (body) => this.succeed(body),
+      error: (e) => this.fail(e),
+    });
+  }
+
+  private start(): void {
+    this.isWorking.set(true);
+    this.result.set(null);
+    this.error.set(null);
+  }
+  private fail(error: object): void {
+    this.isWorking.set(false);
+    this.result.set(null);
+    this.error.set(error);
+  }
+  private succeed(result: T | T[]): void {
+    this.isWorking.set(false);
+    this.result.set(result);
+    this.error.set(null);
+  }
+}
--- src/app/routes/home/home.facade.ts
+++ src/app/routes/home/home.facade.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, computed, inject, signal } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { CommandState } from 'src/app/core/command.state';
 
 @Injectable()
 export class HomeFacade {
@@ -7,47 +8,8 @@ export class HomeFacade {
   #http = inject(HttpClient);
   getActivitiesState = new CommandState<object[]>();
   getActivities(): void {
-    this.getActivitiesState.start();
     const publishedUrl = `${this.#api}?state=published`;
-    this.#http.get<object[]>(publishedUrl).subscribe({
-      next: (body) => this.getActivitiesState.succeed(body),
-      error: (e) => this.getActivitiesState.fail(e),
-    });
-  }
-}
-
-export class CommandState<T> {
-  readonly isWorking = signal(false);
-  readonly error = signal<object | null>(null);
-  readonly result = signal<T | T[] | null>(null);
-  readonly hasError = computed(() => !!this.error());
-  readonly isCompleted = computed(() => {
-    if (this.isWorking()) return false;
-    if (this.error()) return false;
-    return true;
-  });
-  readonly hasContent = computed(() => {
-    const result = this.result();
-    if (!result) return false;
-    if (Array.isArray(result)) return result.length > 0;
-    return true;
-  });
-  readonly errorMessage = computed(() => {
-    const e = this.error();
-    if (!e) return '';
-    return e instanceof Error ? e.message : JSON.stringify(e);
-  });
-  start(): void {
-    this.isWorking.set(true);
-    this.error.set(null);
-    this.result.set(null);
-  }
-  fail(e: object): void {
-    this.isWorking.set(false);
-    this.error.set(e);
-  }
-  succeed(result: T | T[]): void {
-    this.isWorking.set(false);
-    this.result.set(result);
+    const command$ = this.#http.get<object[]>(publishedUrl);
+    this.getActivitiesState.execute(command$);
   }
 }
